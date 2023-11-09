6fdb4536f41fbe1a761521a3e9ff945b496853a6
MobsofFools <edvinlin@msn.com>
2023-11-09 16:35:53 -0700

    access fix

--- SectionVisibility/SectionControl.tsx
+++ SectionVisibility/SectionControl.tsx
@@ -10,7 +10,7 @@ const uglyBlue = '#0066FF'
 const SectionControl = (props:ISectionControlProps)=> {
     const {context, onChange} = props;
     const controlInitialValue = context?.parameters?.controllerValue.raw!;
-    const [controlValue, setControlValue] = useState<boolean>(controlInitialValue? controlInitialValue : false);
+    //const [controlValue, setControlValue] = useState<boolean>(controlInitialValue? controlInitialValue : false);
     const controlRef = useRef(null);
     const color = context?.parameters?.color?.raw! ? context.parameters.color.raw! : uglyBlue;
     const userLang = context?.userSettings.languageId;
@@ -43,12 +43,12 @@ const SectionControl = (props:ISectionControlProps)=> {
             if (event.key === " ") {
                 //event.stopPropagation();
                 event.preventDefault();
-                setControlValue(!controlValue);
+                onChange(!controlInitialValue);
               }
               // If enter is pressed, activate the button
               else if (event.key === "Enter") {
                 event.preventDefault();
-                setControlValue(!controlValue);
+                onChange(!controlInitialValue);
               
             }
         }
@@ -60,32 +60,32 @@ const SectionControl = (props:ISectionControlProps)=> {
             }
         }
     }
-    function handleExpandAllEvent(e:Event){
-        setControlValue(true)
-    }
-    function handleCollapseAllEvent(e:Event){
-        setControlValue(false)
-    }
-    function handleExpandSectionsEvent(e:CustomEvent){
-        console.log(e);
-        const expandArray:Array<string>|null = e.detail;
-        if(expandArray?.includes(`${context?.parameters.controllerValue.attributes?.LogicalName}`)){
-            setControlValue(true);
-        }
-    }
-    function registerExternalExpandCollapseListeners(){
-        window.top?.addEventListener("expandall",handleExpandAllEvent)
-        window.top?.addEventListener("collapseall",handleCollapseAllEvent)
-        window.top?.addEventListener<any>("expandsections",handleExpandSectionsEvent)
-    }
-    function registerInitialExternalExpandCollapseListeners(){
-        setTimeout(()=>{
-            registerExternalExpandCollapseListeners();
-        },5000)
-    }
+    // function handleExpandAllEvent(e:Event){
+    //     onChange(true)
+    // }
+    // function handleCollapseAllEvent(e:Event){
+    //     onChange(false)
+    // }
+    // function handleExpandSectionsEvent(e:CustomEvent){
+    //     console.log(e);
+    //     const expandArray:Array<string>|null = e.detail;
+    //     if(expandArray?.includes(`${context?.parameters.controllerValue.attributes?.LogicalName}`)){
+    //         onChange(true);
+    //     }
+    // }
+    // function registerExternalExpandCollapseListeners(){
+    //     window.top?.addEventListener("expandall",handleExpandAllEvent)
+    //     window.top?.addEventListener("collapseall",handleCollapseAllEvent)
+    //     window.top?.addEventListener<any>("expandsections",handleExpandSectionsEvent)
+    // }
+    // function registerInitialExternalExpandCollapseListeners(){
+    //     setTimeout(()=>{
+    //         registerExternalExpandCollapseListeners();
+    //     },5000)
+    // }
   
     useEffect(()=>{
-        onChange(controlValue)
+        onChange(controlInitialValue)
         const control = document.getElementById(`${context?.parameters.controllerValue.attributes?.LogicalName}_pcf_control`);
         control?.addEventListener('keyup',handleSpaceKeyupEvent)
         control?.addEventListener('keydown',handleAccessibilityKeydownEvent)
@@ -93,19 +93,20 @@ const SectionControl = (props:ISectionControlProps)=> {
             control?.removeEventListener('keydown',handleAccessibilityKeydownEvent)
             control?.removeEventListener('keyup',handleSpaceKeyupEvent)
         }
-    },[controlValue])
-    useEffect(()=>{
-        registerInitialExternalExpandCollapseListeners();
-    },[])
+    },[controlInitialValue])
+    // useEffect(()=>{
+    //     registerExternalExpandCollapseListeners();
+    // },[])
 
     return(
-        <div style={{color:color, borderBottom:'1px solid', borderColor:'rgb(237,235,233)',fontFamily:'SegoeUI-Semibold',display:'flex',alignItems:"center",boxSizing:"border-box", width:"100%"}} tabIndex={0} ref={controlRef} onClick={()=>setControlValue(!controlValue)} id={`${context?.parameters.controllerValue.attributes?.LogicalName}_pcf_control`} aria-label={userLang === 1036? frLabel : engLabel} onKeyUp={handleAccessibilityKeyupEvent} title={userLang === 1036? frLabel : engLabel}>
+        <div style={{color:color, borderBottom:'1px solid', borderColor:'rgb(237,235,233)',fontFamily:'SegoeUI-Semibold',display:'flex',alignItems:"center",boxSizing:"border-box", width:"100%"}} tabIndex={0} ref={controlRef} onClick={()=>onChange(!controlInitialValue)} id={`${context?.parameters.controllerValue.attributes?.LogicalName}_pcf_control`} aria-expanded={controlInitialValue} aria-label={userLang === 1036? frLabel : engLabel} onKeyUp={handleAccessibilityKeyupEvent} title={userLang === 1036? frLabel : engLabel}>
             {
-                controlValue?
+                controlInitialValue?
                 <Icon iconName='CaretDownSolid8'/> :
                 <Icon iconName='CaretRightSolid8'/> // style={{color:color}}
             }
             <h3 style={{color:color, marginLeft:"1rem"}}>
+                {}
                 {userLang === 1036? frLabel : engLabel}
             </h3>
         </div>
